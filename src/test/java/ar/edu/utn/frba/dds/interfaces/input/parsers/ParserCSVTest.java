e562515d007ca632a90d6584685f57958f3d095e
Patricio-Galli <pagalli@frba.utn.edu.ar>
2023-03-29 18:01:12 -0300

    Commit forzoso

--- src/test/java/ar/edu/utn/frba/dds/interfaces/input/parsers/ParserCSVTest.java
+++ src/test/java/ar/edu/utn/frba/dds/interfaces/input/parsers/ParserCSVTest.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.dds.interfaces.input.parsers;
 
 import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,46 @@ public class ParserCSVTest {
 
         Assertions.assertEquals("hola", pruebas.get(0).getFoo());
         Assertions.assertEquals(456, pruebas.get(1).getBar());
+    }
+
+    @Test
+    void laLibreriaPuedeTransformarCSVEnInstanciaConPosicion() throws IOException {
+        List<PruebaConOrden> pruebas = new ParserCSV<>(PruebaConOrden.class)
+                .parseFileToCollection("src/test/resources/ParserCsv.csv");
+
+        Assertions.assertEquals("hola", pruebas.get(0).getFoo());
+        Assertions.assertEquals(456, pruebas.get(1).getBar());
+    }
+
+    public static class PruebaConOrden {
+        @CsvBindByPosition(position = 1)
+        private Integer bar;
+        @CsvBindByPosition(position = 0)
+        private String foo;
+
+        public PruebaConOrden() {
+        }
+
+        public PruebaConOrden(String foo, Integer bar) {
+            this.foo = foo;
+            this.bar = bar;
+        }
+
+        public String getFoo() {
+            return foo;
+        }
+
+        public void setFoo(String foo) {
+            this.foo = foo;
+        }
+
+        public Integer getBar() {
+            return bar;
+        }
+
+        public void setBar(Integer bar) {
+            this.bar = bar;
+        }
     }
 
     public static class PruebaConNombres {
@@ -48,5 +89,4 @@ public class ParserCSVTest {
             this.bar = bar;
         }
     }
-
 }
